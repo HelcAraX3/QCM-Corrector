f6300b1eaf2712bd6fb9845e03a33b5a86e01c0a
h4x3 <gautier.valentin@h4x3.ovh>
2022-03-21 20:42:52 +0100

    Test sur opencv

--- Classe_classe.java
+++ Classe_classe.java
@@ -1,3 +1,3 @@
 public class Classe_classe {
-    int conter;
+    int conteur;
 }
--- test.java
+++ test.java
@@ -1,6 +1,6 @@
-class test
+public class test
 {
-    static void main(String[ ] args)
+    public static void main(String[ ] args)
     { System.out.println("Bonjour !");
     }
 }
